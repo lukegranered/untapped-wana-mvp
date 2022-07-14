ce5545b5c90d53ecee63eda59c050e440b7d95f9
Luke G <lukejgranered@gmail.com>
2022-07-14 09:42:34 -0500

    text area refactored to use kendoreact

--- src/components/NotesSection.jsx
+++ src/components/NotesSection.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { TextArea } from '@progress/kendo-react-inputs';
 
 export const NotesSection = () => {
+    const [notes, setNotes] = useState('');
   return (
-    <div className='k-label k-mb-3'>
-        <label>Additional Notes</label>
-        <textarea name='notes' id='notes' placeholder='Enter additional notes'></textarea>
+    <div className='k-my-8'>
+        <form className='k-form k-mb-4'>
+            <label className='k-label k-mb-3'>Additional Notes</label>
+            <TextArea data={notes} onChange={e => setNotes(e.value)} />
+        </form>
     </div>
   )
 }
